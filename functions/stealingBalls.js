cc7563f18194e031ec2f212c33c7661816b60bee
Denis <denisskholmov@gmail.com>
2025-02-27 15:14:58 +0300

    first commit

---
+++ functions/stealingBalls.js
@@ -0,0 +1,27 @@
+import {sumItems, generationIndexForArray} from "../helpers/helpers.js";
+
+export const stealingBalls = (maxBalls, warehouseBall) => {
+    if(warehouseBall.length === 0){
+        return "грабитель попал на пустой склад. Облом!";
+    }
+
+    let ballsInBag = 0;
+    let robbersBag = [];
+    let sumWarehouseBall = sumItems(warehouseBall);
+    
+
+    for(let i = 0; i<warehouseBall.length; i++){
+        robbersBag[i] = Math.floor(warehouseBall[i]/sumWarehouseBall * maxBalls); 
+        ballsInBag = ballsInBag + robbersBag[i];
+    }
+
+
+    
+    if(maxBalls!==ballsInBag){
+        const index = generationIndexForArray(robbersBag.length-1);
+        robbersBag[index]=robbersBag[index] + 1;
+    }
+
+    return robbersBag;
+}
+
