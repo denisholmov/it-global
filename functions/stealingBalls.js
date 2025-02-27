aea693e53c641ebdc2275c71f63ddd52ae339f58
Denis <denisskholmov@gmail.com>
2025-02-27 23:16:13 +0300

    Добавил условие на проверку всех цветов, исправил распределение всех шаров по новому массиву

--- functions/stealingBalls.js
+++ functions/stealingBalls.js
@@ -1,29 +1,41 @@
-import {sumItems, generationIndexForArray} from "../helpers/helpers.js";
+import { sumItems, generationIndexForArray } from '../helpers/helpers.js';
 
 export const stealingBalls = (maxBalls, warehouseBall) => {
-    if(warehouseBall.length === 0){
-        return "грабитель попал на пустой склад. Облом!";
+  if (warehouseBall.length === 0) {
+    return 'грабитель попал на пустой склад. Облом!';
+  }
+
+  let sumBag = 0;
+  let robbersBag = [];
+  let sumWarehouseBall = sumItems(warehouseBall);
+
+  // распределяю шары по новому массиву
+  for (let i = 0; i < warehouseBall.length; i++) {
+    if (sumBag < maxBalls) {
+      robbersBag[i] = Math.floor((warehouseBall[i] / sumWarehouseBall) * maxBalls) || 0;
+      sumBag = sumBag + robbersBag[i];
     }
+  }
 
-    let sumBag = 0;
-    let robbersBag = [];
-    let sumWarehouseBall = sumItems(warehouseBall);
-    
+  // добавляю недостающие шарики
+  while (sumBag < maxBalls) {
+    const index = generationIndexForArray(robbersBag.length - 1);
+    robbersBag[index] += 1;
+    sumBag += 1;
+  }
 
-    for(let i = 0; i<maxBalls && warehouseBall.length; i++){
-        if(sumBag<maxBalls){
-            robbersBag[i] = Math.floor(warehouseBall[i]/sumWarehouseBall * maxBalls) || 1; 
-            sumBag = sumBag + robbersBag[i];
+  // проверяю, все ли цвета представлены в новом массиве
+  for (let i = 0; i < robbersBag.length; i++) {
+    if (robbersBag[i] === 0) {
+      for (let j = 0; j < robbersBag.length; j++) {
+        if (robbersBag[j] > 1) {
+          robbersBag[j] -= 1;
+          robbersBag[i] += 1;
+          break;
         }
+      }
     }
+  }
 
-
-    
-    if(maxBalls<sumBag){
-        const index = generationIndexForArray(robbersBag.length-1);
-        robbersBag[index]=robbersBag[index] + 1;
-    }
-
-    return robbersBag;
-}
-
+  return robbersBag;
+};
--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-import {stealingBalls} from "./functions/stealingBalls.js";
+import { stealingBalls } from './functions/stealingBalls.js';
 
-console.log(stealingBalls(12, [10, 500, 30, 40, 50]));
+console.log(stealingBalls(12, [10, 500, 30, 40, 50, 30, 1, 1, 5000]));
 console.log(stealingBalls(12, [10, 20, 30]));
-
