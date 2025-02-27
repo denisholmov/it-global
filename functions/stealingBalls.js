7c2ecbb244576ca8a9930a137b490caf5f80b8e1
Denis <denisskholmov@gmail.com>
2025-02-27 15:59:31 +0300

    исправил ограничение цвета

--- functions/stealingBalls.js
+++ functions/stealingBalls.js
@@ -5,25 +5,21 @@ export const stealingBalls = (maxBalls, warehouseBall) => {
         return "грабитель попал на пустой склад. Облом!";
     }
 
-    const newWarehouseBall = [];
+    let sumBag = 0;
+    let robbersBag = [];
+    let sumWarehouseBall = sumItems(warehouseBall);
+    
 
-    for(let i = 0; i < maxBalls && i < warehouseBall.length; i++){
-        newWarehouseBall.push(warehouseBall[i]);
+    for(let i = 0; i<maxBalls && warehouseBall.length; i++){
+        if(sumBag<maxBalls){
+            robbersBag[i] = Math.floor(warehouseBall[i]/sumWarehouseBall * maxBalls) || 1; 
+            sumBag = sumBag + robbersBag[i];
+        }
     }
 
-    let ballsInBag = 0;
-    const robbersBag = [];
-    const sumWarehouseBall = sumItems(newWarehouseBall);
-    
-
-    for(let i = 0; i<newWarehouseBall.length; i++){
-        robbersBag[i] = Math.floor(newWarehouseBall[i]/sumWarehouseBall * maxBalls); 
-        ballsInBag = ballsInBag + robbersBag[i];
-    }
 
     
-    
-    if(maxBalls!==ballsInBag){
+    if(maxBalls<sumBag){
         const index = generationIndexForArray(robbersBag.length-1);
         robbersBag[index]=robbersBag[index] + 1;
     }
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import {stealingBalls} from "./functions/stealingBalls.js";
 
-console.log(stealingBalls(12, [10, 20, 30, 49]));
+console.log(stealingBalls(12, [10, 500, 30, 40, 50]));
 console.log(stealingBalls(12, [10, 20, 30]));
 
